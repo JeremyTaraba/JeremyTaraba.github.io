fd692ec848f9d853dbf495a15f67cc84bb34f89f
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-23 02:22:24 -0700

    navbar hide

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ $(document).ready(function(){
   })
 
   // scroll functions
-$(window).scroll(function(e) {
+  $(window).scroll(function(e) {
 
-  // add/remove class to navbar when scrolling to hide/show
-  $('.navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
 
 });
