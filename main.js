ad58e1aa12e8cebed7920fd6ca9d7a71ecb4e5f8
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-23 03:06:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+  })
+
   // scroll functions
   $(document).ready(function () {
     var previousScroll = 0;
@@ -14,11 +18,11 @@
         previousScroll = currentScroll;
       }
     });
-  
+
     function hideNav() {
       $(".navbar").removeClass("is-visible").addClass("is-hidden");
     }
-  
+
     function showNav() {
       $(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
     }
