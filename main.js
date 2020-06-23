c0c9a35b2a28550c00c76274b2421af8dfb0cd8b
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-23 02:33:51 -0700

    scroll comes back

--- main.js
+++ main.js
@@ -3,14 +3,27 @@ $(document).ready(function(){
   })
 
   // scroll functions
-  $(window).scroll(function(e) {
+  $(document).ready(function () {
+    var previousScroll = 0;
+    $(window).scroll(function () {
+      var currentScroll = $(this).scrollTop();
+      if (currentScroll < 100) {
+        showTopNav();
+      } else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+        if (currentScroll > previousScroll) {
+          hideNav();
+        } else {
+          showNav();
+        }
+        previousScroll = currentScroll;
+      }
+    });
 
-    // add/remove class to navbar when scrolling to hide/show
-    var scroll = $(window).scrollTop();
-    if (scroll >= 150) {
-        $('.navbar').addClass("navbar-hide");
-    } else {
-        $('.navbar').removeClass("navbar-hide");
+    function hideNav() {
+      $(".navbar").removeClass("is-visible").addClass("is-hidden");
     }
 
+    function showNav() {
+      $(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+    }
 });
