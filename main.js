ae323fc2772cf3a3f72656315eab5ddf633f7ff9
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-23 02:17:59 -0700

    scroll

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
   })
+
+  // scroll functions
+$(window).scroll(function(e) {
+
+  // add/remove class to navbar when scrolling to hide/show
+  $('.navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
+
+});
