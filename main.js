c9e6fadc3978c0c95b1d1ce1995694c1d339c1fc
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-23 19:33:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $('.nav-link').click(function(){    
     var divId = $(this).attr('href');
      $('html, body').animate({
-      scrollTop: $(divId).offset().top - 54
+      scrollTop: $(divId).offset().top - 70
     }, 100);
   })
 
