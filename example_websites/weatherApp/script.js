9e0572cce962ef98386a071d79d9aa7c559c1e19
Jeremy Taraba <jeremytaraba@gmail.com>
2024-05-03 17:10:09 -0700

    working on ethan and maya websites

--- example_websites/weatherApp/script.js
+++ example_websites/weatherApp/script.js
@@ -9,9 +9,13 @@ $("#location_form").submit(function(e) {
     e.preventDefault();
 });
 
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 locationInput = document.getElementById('input-datalist');
 cityList = document.getElementById("list-cities");
 cityList.innerHTML = "";
+weatherForm = document.getElementById("weather-form");
+redirectLink = document.getElementById("redirectLink");
 
 temp = [];
 addCity("irvine")
@@ -25,18 +29,27 @@ city_name = ""
 url = `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=5&appid=e0df3dbc9bda8f59a4d31245ef415996`
 
 locationInput.addEventListener("input" , event => {
-    //event.preventDefault();
     city_name = locationInput.value;
     console.log(city_name);
+   
 })
 
-// locationInput.addEventListener("submit" , event => {
-//     event.preventDefault();
-    
-// })
+weatherForm.addEventListener("submit" , event => {
+    event.preventDefault();
+    console.log("pressed submit");  
+    if(locationInput.value != ""){
+        document.location.href="showWeather/index.html";
+    }
+    else{
+        alert("Please enter a city name");
+    }
+
+})
 
 function addCity(city) {
     singleCity = document.createElement('option');
     singleCity.textContent = city;
     cityList.appendChild(singleCity);
 }
+
+
