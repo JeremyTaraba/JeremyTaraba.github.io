3a7c2f5cefea9fc22a4552d0541d8cc35b7cca77
Jeremy Taraba <jeremytaraba@gmail.com>
2024-07-26 17:39:22 -0700

    fd

--- example_websites/cookieClicker/signUpPage/script.js
+++ example_websites/cookieClicker/signUpPage/script.js
@@ -1,28 +1,28 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
 
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyANEFOuK92IBNEVcIVGW9LfAJsiBWG6sog",
-  authDomain: "cookieclicker-72b05.firebaseapp.com",
-  projectId: "cookieclicker-72b05",
-  storageBucket: "cookieclicker-72b05.appspot.com",
-  messagingSenderId: "766996351721",
-  appId: "1:766996351721:web:05e35fc69cdd19d434fe78",
-  measurementId: "G-24ZMJNCH1R"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyANEFOuK92IBNEVcIVGW9LfAJsiBWG6sog",
+//   authDomain: "cookieclicker-72b05.firebaseapp.com",
+//   projectId: "cookieclicker-72b05",
+//   storageBucket: "cookieclicker-72b05.appspot.com",
+//   messagingSenderId: "766996351721",
+//   appId: "1:766996351721:web:05e35fc69cdd19d434fe78",
+//   measurementId: "G-24ZMJNCH1R"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth();
+// const auth = getAuth();
 
 
 // createUserWithEmailAndPassword(auth, email, password)
