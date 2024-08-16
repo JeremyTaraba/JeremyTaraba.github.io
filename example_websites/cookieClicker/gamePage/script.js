62ed3c289068171b132d0ca1fd376ab5d3385234
Jeremy Taraba <jeremytaraba@gmail.com>
2024-08-16 14:44:01 -0700

    Update script.js

--- example_websites/cookieClicker/gamePage/script.js
+++ example_websites/cookieClicker/gamePage/script.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13/firebase-app-compat.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13/firebase-firestore-compat.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
